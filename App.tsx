63fedc12158c30f65994c04e3ce8254fefa9bd23
kambala3000 <kambala02@gmail.com>
2018-03-06 12:11:04 +0200

    added antd ui library, fixed components import

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'antd-mobile';
 
 export default class App extends React.Component<{}> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Проснись, ты обосрался</Text>
+        <Button>Укатиться в закат</Button>
       </View>
     );
   }
