8ad05a314be18998da2351f0d4b05f74ad128f4e
kambala3000 <kambala02@gmail.com>
2018-05-03 23:14:00 +0300

    check inviteKey #1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, RouteComponentProps } from 'react-router';
+import { AxiosResponse } from 'axios';
+
+// api
+import roomsApi from 'api/rooms';
 
 // components
 import Routes from './routes';
@@ -10,11 +14,13 @@ import { getProfile } from 'store/Auth/actions';
 
 // types
 import { IUserData } from 'store/Auth/types';
+import { IGlobalStore } from 'store/types';
 
 // styles
 import 'styles/main.css';
 
 interface IProps extends RouteComponentProps<any> {
+  isUserLoaded: boolean;
   getProfile: () => Promise<IUserData>;
 }
 
@@ -22,6 +28,8 @@ class App extends React.Component<IProps> {
   async componentDidMount() {
     const { getProfile, history } = this.props;
     const { pathname } = history.location;
+
+    console.log(localStorage.getItem('inviteKey'));
 
     try {
       const profile = await getProfile();
@@ -34,9 +42,47 @@ class App extends React.Component<IProps> {
     }
   }
 
+  checkStorageForInvite = async () => {
+    const inviteKey = localStorage.getItem('inviteKey');
+    if (inviteKey) {
+      const response: AxiosResponse<any> = await roomsApi.verifyInvite(
+        inviteKey
+      );
+    }
+  };
+
+  parseQueryParams = async () => {
+    const { isUserLoaded, location } = this.props;
+    const { search } = location;
+
+    if (search) {
+      const searchParams: any = {};
+      const query: any = new URLSearchParams(search);
+
+      for (let param of query.entries()) {
+        searchParams[param[0]] = param[1];
+      }
+
+      if (searchParams.invite) {
+        if (!isUserLoaded) {
+          localStorage.setItem('inviteKey', searchParams.invite);
+          return;
+        }
+
+        const response: AxiosResponse<any> = await roomsApi.verifyInvite(
+          searchParams.invite
+        );
+      }
+    }
+  };
+
   render() {
     return <Routes />;
   }
 }
+
+const mapStateToProps = ({ auth }: IGlobalStore) => ({
+  isUserLoaded: auth.isUserLoaded
+});
 
 export default withRouter(connect<any, any, any>(null, { getProfile })(App));
